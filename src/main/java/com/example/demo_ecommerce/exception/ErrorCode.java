be0b575dd1f4d6251f559eca9c0d299fafd61718
hanbui3001 <bronzegoemo@gmail.com>
2026-06-12 22:21:58 +0700

    Add JSON handlers for unauthorized and forbidden JWT responses

--- src/main/java/com/example/demo_ecommerce/exception/ErrorCode.java
+++ src/main/java/com/example/demo_ecommerce/exception/ErrorCode.java
@@ -11,10 +11,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USER_EXISTED(409, "User existed", HttpStatus.CONFLICT),
     USER_NOT_FOUND(404, "User not found", HttpStatus.NOT_FOUND),
-    UNAUTHENTICATED(401, "Unauthorized", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(401, "Unauthorized", HttpStatus.UNAUTHORIZED),
     GENERATE_JWT_ERROR(500, "Generate JWT Error", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_JWT_TOKEN(502, "Only Access Token Is Allow", HttpStatus.UNAUTHORIZED),
     ROLE_INVALID(400,  "Invalid Role", HttpStatus.BAD_REQUEST),
+    FORBIDDEN(403,  "Forbidden", HttpStatus.FORBIDDEN),
     ;
     private int code;
     private String message;
