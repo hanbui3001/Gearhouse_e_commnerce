0220c256eef46462fb7eefc3859045caf987a67a
Bùi Văn Hân <hanvanbui3001@gmail.com>
2026-07-08 13:33:40 +0700

    add feature: create table product image

---
+++ server/src/main/java/com/example/demo_ecommerce/model/ProductImage.java
@@ -0,0 +1,22 @@
+package com.example.demo_ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product_images")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    @Column(nullable = false, name = "image_url")
+    String imageUrl;
+    @Column(name = "display_order")
+    Integer displayOrder;
+}
